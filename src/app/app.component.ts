60682cb392a7718b2ed4da7d4508719380182c51
Adrian <jorgeadrianpages98@gmail.com>
2023-04-03 20:09:16 +0200

    03/04/23 ejemplos de operadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,43 @@ export class AppComponent {
 
     console.log("yeeeee");
 
+    //esto itera el array ejecutando la funcion del parametro del map en cada elemento
+    const auxMap = [1,2,3,4,5].map(num => num * 2);
+    console.log("auxMap: ", auxMap);
+
+    //a diferencia del map el foreach no mete en la variable lo que hayamos hecho, solo lo recorre 
+    const auxForEach = [1,2,3,4,5].forEach(num => num * 2);
+    console.log("auxForEach", auxForEach);
+
+    //si encuentra lo que buscamos lo devuelve y si no devuelve undefined
+    const auxFind = [1,2,3,4,5].find(num => num === 3)
+    console.log("auxFind: ", auxFind);
+
+    //devuelve el indice de lo que buscamos si no lo encuentra devuelve -1
+    const auxFindIndex = [1,2,3,4,5].findIndex(num => num === 7)
+    console.log("auxFindIndex: ", auxFindIndex);
+
+    //lo mismo que antes pero sin arrow, tambie nsirve para buscar en cadenas
+    const auxIndexOf = [1,2,3,4,5].indexOf(3)
+    console.log(auxIndexOf);
+    //devuelve la posicion de inicio de la subcadena que buscamos y -1 si no esta
+    const auxIndexOf2 = "hola mundo".indexOf("mundi")
+    console.log(auxIndexOf2)
+
+    //crea y devuelve un array con los elementos del array que cumplen la condicion de la arrow, si no encuentra nada devuelve el array vacio
+    const auxFilter = [1,2,3,4,5].filter(num => num % 2 !== 0);
+     console.log("auxFilter: ", auxFilter);
+
+     //convierte un array a cadena separando los elementos con la cadena del parametro, para no separar pasamos cadena vacia
+     const auxJoin =[1,2,3,4,5].join('-')
+     console.log("auxJoin: ",auxJoin);
+     
+     //el inverso del join
+     const auxSplit = "1-2-3-4-5".split('-');
+     console.log("auxSplit: ", auxSplit);
+     
+     
+    
   }
 
   //ejemplo de funcion en typescript
