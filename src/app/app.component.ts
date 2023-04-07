c0c0344a27e9474d26affc4c671ad27a9dc8537b
JAdrianP <jorgeadrianpages98@gmail.com>
2023-04-07 12:36:26 +0200

    ejemplo de viewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testAngular';
+
+  //esta es la manera de enlazar variables del HTML al TS, las variables HTML son las que llevan # delante
+  //si el static estuviera a true, en caso de estar la variable my text enlazada a un *ngIf y el el elemento 
+  //de la variable no estueviera renderizado, daria error, el valor por defecto de static es true
+  @ViewChild('myText', {static: false}) myText!: ElementRef;
+  //@ViewChild('myText') myText!: ElementRef;
+
+  send(data:any){
+
+    console.log(this.myText.nativeElement.value);
+    
+  }
 }
