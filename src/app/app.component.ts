66cbd7c7d8df604870726988364367d6fe12b0cd
JAdrianP <jorgeadrianpages98@gmail.com>
2023-04-07 19:28:59 +0200

    ni caso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testAngular';
+
+
+  constructor(){
+
+    console.log("--------------funciones puras----------------");
+    
+    console.log(this.pureExample(3,4));
+    console.log(this.pureExample(2,5));
+    console.log(this.pureExample(4,4));
+    console.log(this.pureExample(9,1));
+    
+    console.log("--------------funciones impuras----------------");
+
+    console.log(this.impureExample(9,1))
+    console.log(this.impureExample(9,1))
+    console.log(this.impureExample(9,1))
+    console.log(this.impureExample(9,1))
+
+  }
+
+  pureExample(a:number, b:number):number{
+    return a+b
+  }
+
+  impureExample(a:number,b:number):number{
+
+    return a + b + Math.round(10 * Math.random());
+  }
 }
